6039a191b274692621486493635f8817c130fe6a
柠乐 <1204546769@qq.com>
2022-07-12 09:12:01 +0800

    dfg

--- contracts/NoDelegateCall.sol
+++ contracts/NoDelegateCall.sol
@@ -8,11 +8,9 @@ abstract contract NoDelegateCall {
     address private immutable original;
 
     constructor() {
-        // Immutables are computed in the init code of the contract, and then inlined into the deployed bytecode.
-        // In other words, this variable won't change when it's checked at runtime.
         original = address(this);
     }
-
+    //1111
     /// @dev Private method is used instead of inlining into modifier because modifiers are copied into each method,
     ///     and the use of immutable means the address bytes are copied in every place the modifier is used.
     function checkNotDelegateCall() private view {
